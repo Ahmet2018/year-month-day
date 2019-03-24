51b1bfba83ac3ae088ae05b6a60f3713c895b4f9
macbookair <macbookair@192.168.100.18>
2019-03-24 15:28:40 +0300

    year month day

---
+++ src/yearmonthday/YearMonthDay.java
@@ -0,0 +1,23 @@
+
+package yearmonthday;
+
+import java.util.Scanner;
+
+public class YearMonthDay {
+
+       public static void main(String[] args) {
+           Scanner date = new Scanner (System.in);
+           System.out.println(" Введите дату");
+           int first,second;
+           
+           
+           
+           
+                   
+                   
+           
+           
+           
+    }
+    
+}
